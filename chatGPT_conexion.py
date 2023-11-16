0c84b8cd82dde484b5ec1c7458f1ab6c5347bc2b
ALEXA MORALES PEREZ VARGAS <amoral98@itam.mx>
2023-11-16 07:39:22 -0600

    borrado de la llave.

--- chatGPT_conexion.py
+++ chatGPT_conexion.py
@@ -2,7 +2,7 @@ import openai
 
 
 def conexion(prompt):
-    openai.api_key = "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"
+    openai.api_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
     completion = openai.chat.completions.create(
         model="gpt-4",
