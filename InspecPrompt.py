d7d494370910f9747e0d2f904c911c81b7738546
Fernando Retama <lrodr126@itam.mx>
2023-11-16 10:51:44 -0600

    añadí stopword en inglés

--- InspecPrompt.py
+++ InspecPrompt.py
@@ -1,15 +1,17 @@
 import string
-
 from nltk.corpus import stopwords
 
 # no se si deberíamos también bajar las de inglés
 stopwords_espanol = stopwords.words("spanish")
+stopwords_ingles = stopwords.words("english")
 
 
-def limpiaTexto(prompt: str) -> list:
+def limpiaTexto(prompt: str) -> set:
     # eliminación de puntuación y números y pase a minúsculas
     prompt = prompt.translate(str.maketrans("", "", string.punctuation)).lower()
     # separación de palabras
-    palabras = prompt.split(" ")
-    res = [p for p in palabras if p not in stopwords_espanol]
+    palabras = set(prompt.split(" "))
+    res = [
+        p for p in palabras if p not in stopwords_espanol and p not in stopwords_ingles
+    ]
     return res
