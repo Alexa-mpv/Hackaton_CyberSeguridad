10d4cd4cb2c3d1a873f374fe598ea8f5980fff69
Fernando Retama <lrodr126@itam.mx>
2023-11-16 18:22:11 -0600

    limpiador de prompt terminado

--- InspecPrompt.py
+++ InspecPrompt.py
@@ -5,7 +5,7 @@ stopwords_espanol = stopwords.words("spanish")
 stopwords_ingles = stopwords.words("english")
 
 
-def limpiaTexto(prompt: str) -> set:
+def limpiaTexto(prompt: str) -> list:
     # eliminación de puntuación y números y pase a minúsculas
     prompt = prompt.translate(str.maketrans("", "", string.punctuation)).lower()
     # separación de palabras
@@ -14,3 +14,10 @@ def limpiaTexto(prompt: str) -> set:
         p for p in palabras if p not in stopwords_espanol and p not in stopwords_ingles
     ]
     return res
+
+
+print(
+    limpiaTexto(
+        "Este en un prompt bien fachero sin información sensible definitivamente, en serio. Tengo 2 vacas."
+    )
+)
