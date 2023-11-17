b5dd0515fa17b7fb4d79c29d92c42f4f79ee5de8
Fernando Retama <lrodr126@itam.mx>
2023-11-16 22:42:35 -0600

    No se eliminan todos los caracteres especiales

--- InspecPrompt.py
+++ InspecPrompt.py
@@ -6,18 +6,19 @@ stopwords_ingles = stopwords.words("english")
 
 
 def limpiaTexto(prompt: str) -> list:
-    # eliminación de puntuación y números y pase a minúsculas
-    prompt = prompt.translate(str.maketrans("", "", string.punctuation)).lower()
-    # separación de palabras
-    palabras = set(prompt.split(" "))
+    # elimino comas, dos puntos y punto y comas
+    prompt = prompt.translate(str.maketrans("", "", ",:;"))
+    # pase a minúsculas y separacion
+    prompt = prompt.lower().split(" ")
+    # edepuración de stopwords
     res = [
-        p for p in palabras if p not in stopwords_espanol and p not in stopwords_ingles
+        p for p in prompt if p not in stopwords_espanol and p not in stopwords_ingles
     ]
     return res
 
 
 print(
     limpiaTexto(
-        "Este en un prompt bien fachero sin información sensible definitivamente, en serio. Tengo 2 vacas."
+        "Este es un prompt bien fachero sin información sensible definitivamente, en serio. Tengo 2 vacas."
     )
 )
