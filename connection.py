4812aca2544d09801877843e360c1f90a80eb5cb
ALEXA MORALES PEREZ VARGAS <amoral98@itam.mx>
2023-11-17 21:45:02 -0600

    actulización de la función de conexión

--- connection.py
+++ connection.py
@@ -2,31 +2,17 @@ import openai
 import requests
 
 
-def conexion(prompt, ai):
+def conexion(prompt):
     """Función que se encarga de conectar con la API de la IA seleccionada y regresa la respuesta de la IA."""
 
-    if ai == "chatgpt":
-        openai.api_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-        completion = openai.chat.completions.create(
-            model="gpt-4",
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt,
-                },
-            ],
-        )
-        print(completion)
-    elif ai == "copilot":
-        url = "https://api.cognitive.microsoft.com/bing/v7.0/search"
-        headers = {
-            "Ocp-Apim-Subscription-Key": "api_key",
-        }
-        params = {
-            "prompt": prompt,
-        }
-        response = requests.get(url, headers=headers, params=params)
-        data = response.json()
-        print(data)
-    else:
-        print("No se ha seleccionado ninguna IA")
+    openai.api_key = "xxxxxxxx"
+    completion = openai.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+    )
+    return completion["choices"][0]["message"]["content"]
